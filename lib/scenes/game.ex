10c7ec9d4aeae9c2fa92a45adcf7690f10dc1267
alacaba <alacaba@fastmail.com>
2020-01-25 13:06:02 +0800

    Moving the snake

--- lib/scenes/game.ex
+++ lib/scenes/game.ex
@@ -10,6 +10,7 @@ defmodule ExSnake.Scene.Game do
   @tile_size 32
   @snake_starting_size 5
   @tile_radius 8
+  @frame_ms 128
 
   def init(_arg, opts) do
     viewport = opts[:viewport]
@@ -24,11 +25,15 @@ defmodule ExSnake.Scene.Game do
       trunc(vp_tile_height / 2)
     }
 
+    {:ok, timer} = :timer.send_interval(@frame_ms, :frame)
+
     state = %{
       viewport: viewport,
       tile_width: vp_tile_width,
       tile_height: vp_tile_height,
       graph: @graph,
+      frame_count: 1,
+      frame_timer: timer,
       score: 0,
       objects: %{
         snake: %{
@@ -39,15 +44,21 @@ defmodule ExSnake.Scene.Game do
       }
     }
 
-    state = Map.put(state, :graph,
-      state.graph
-      |> draw_score(state.score)
-      |> draw_game_objects(state.objects))
-
-    {:ok, state, push: state.graph}
+    {:ok, state}
   end
 
-  def draw_score(graph, score) do
+  def handle_info(:frame, %{frame_count: frame_count} = state) do
+    state = move_snake(state)
+
+    graph =
+      state.graph
+      |> draw_game_objects(state.objects)
+      |> draw_score(state.score)
+
+    {:noreply, %{state | frame_count: frame_count + 1}, push: graph}
+  end
+
+  defp draw_score(graph, score) do
     graph
     |> text("Score: #{score}", fill: :white, translate: {@tile_size, @tile_size})
   end
@@ -67,5 +78,18 @@ defmodule ExSnake.Scene.Game do
   def draw_tile(graph, x, y, opts) do
     tile_opts = Keyword.merge([fill: :white, translate: {x * @tile_size, y * @tile_size}], opts)
     graph |> rrect({@tile_size, @tile_size, @tile_radius}, tile_opts)
+  end
+
+  defp move_snake(%{objects: %{snake: snake}} = state) do
+    [head | _] = snake.body
+    new_head_pos = move(state, head, snake.direction)
+
+    new_body = Enum.take([new_head_pos | snake.body], snake.size)
+
+    put_in(state, [:objects, :snake, :body], new_body)
+  end
+
+  defp move(%{tile_width: w, tile_height: h}, {pos_x, pos_y}, {vec_x, vec_y}) do
+    {rem(pos_x + vec_x + w, w), rem(pos_y + vec_y + h, h)}
   end
 end
